2d6fda86b92455dc918105f21c34c20a1f9e45a5
Andrea Lobo <andrea.lobo@pix4d.com>
2023-07-05 18:54:36 +0200

    Printable CV v4

--- pdf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pandoc http://localhost:4000/about/ -o cv.pdf -
--css _sass/cv-print.scss -V title:"" -t html5 -V
-margin-top=15 -V margin-left=10 -V margin-right=1
-0 -V margin-bottom=10 -V papersize:a4 --pdf-engin
-e=wkhtmltopdf
